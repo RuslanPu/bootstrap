9b8e872942a2ef2b7aa0d5669de5602c66da91bf
Ruslan <ruslanpunin@yande.ru>
2020-04-12 17:00:37 +0300

    get user data from getPrincipal, get Role from getAuthorities(). do in header on userPage present user data from Model User

--- src/main/java/preproject9/bootstrap/controller/UserController.java
+++ src/main/java/preproject9/bootstrap/controller/UserController.java
@@ -1,23 +1,34 @@
 package preproject9.bootstrap.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import preproject9.bootstrap.model.Role;
+import preproject9.bootstrap.model.User;
+import preproject9.bootstrap.service.UserService;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
-    @RequestMapping(value = "/userPage", method = RequestMethod.GET)
-    public String userPage(ModelMap model) {
 
-        List<String> messages = new ArrayList<>();
-        messages.add("Hello!");
-        messages.add("This user");
-        model.addAttribute("messages", messages);
+    @Autowired
+    private UserService service;
+
+    @RequestMapping(value = "/userPage", method = RequestMethod.GET)
+    public String userPage(ModelMap model, Authentication auth) {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        List<Role> rolesUser =(List<Role>) SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+        user.setRoles(rolesUser);
+        model.addAttribute("user", user);
         return "user/userPage";
     }
 }
